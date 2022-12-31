778ff6f6b0637161455fbd4b8fc45e3ce26022bd
Oliver Nutzenberger <fhs47768@fh-salzburg.ac.at>
2022-12-31 18:05:23 +0100

    mobile finished

--- src/js/lib/mainnav.js
+++ src/js/lib/mainnav.js
@@ -12,4 +12,14 @@ export function init() {
             button.classList.remove('open');
         }
     });
+
+    const link = nav.querySelectorAll("a");
+    
+    for(var i = 0; i < link.length; i++) {
+        link[i].addEventListener("click" , e => {
+            console.log(button.classList)
+            button.classList.add('closed');
+            button.classList.remove('open');
+        })
+    }
 }
